726f4142c93748cde93276dc695dee51e30b941a
Matthew Kurniawan <78832179+ilze3e3@users.noreply.github.com>
2021-06-16 13:17:53 +1000

    Added more comments to the code

--- Assets/MyScripts/ShowHideInstruction.cs
+++ Assets/MyScripts/ShowHideInstruction.cs
@@ -11,13 +11,18 @@ public class ShowHideInstruction : MonoBehaviour
     [SerializeField] Vector3 posOfButtonWhenShowing = new Vector3(196.7f, 1639.2f,0);
     [SerializeField] Vector3 posOfButtonWhenHidden = new Vector3(196.7f, 2086, 0);
     bool isInstructionShowing = true;
-
+    /// <summary>
+    /// If instructions are hidden show them otherwise hide instruction if it is showing.
+    /// </summary>
     public void ShowOrHideInstruction()
     {
         if (isInstructionShowing) HideInstruction();
         else ShowInstruction();
     }
 
+    /// <summary>
+    /// Show instructions
+    /// </summary>
     public void ShowInstruction()
     {
         
@@ -27,7 +32,9 @@ public class ShowHideInstruction : MonoBehaviour
         button.GetComponentInChildren<Text>().text = "Hide Instructions";
         isInstructionShowing = true;
     }
-
+    /// <summary>
+    /// Hide instructions
+    /// </summary>
     public void HideInstruction()
     {
   
